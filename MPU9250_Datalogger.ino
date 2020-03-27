d4255f86eefb6aaee25e06e920b6c9c436bc0cdc
Wdpckr <31241907+wdpckr92@users.noreply.github.com>
2020-03-27 17:47:57 +0100

    Add files via upload

---
+++ MPU9250_Datalogger.ino
@@ -0,0 +1,443 @@
+/* MPU9250 Basic Example Code
+ by: Kris Winer
+ date: April 1, 2014
+ license: Beerware - Use this code however you'd like. If you
+ find it useful you can buy me a beer some time.
+ Modified by Brent Wilkins July 19, 2016
+
+ Demonstrate basic MPU-9250 functionality including parameterizing the register
+ addresses, initializing the sensor, getting properly scaled accelerometer,
+ gyroscope, and magnetometer data out. Added display functions to allow display
+ to on breadboard monitor. Addition of 9 DoF sensor fusion using open source
+ Madgwick and Mahony filter algorithms. Sketch runs on the 3.3 V 8 MHz Pro Mini
+ and the Teensy 3.1.
+
+ SDA and SCL should have external pull-up resistors (to 3.3V).
+ 10k resistors are on the EMSENSR-9250 breakout board.
+
+ Hardware setup:
+ MPU9250 Breakout --------- Arduino
+ VDD ---------------------- 3.3V
+ VDDI --------------------- 3.3V
+ SDA ----------------------- A4
+ SCL ----------------------- A5
+ GND ---------------------- GND
+ */
+
+#include "quaternionFilters.h"
+#include "MPU9250.h"
+#include "SD.h"
+#include "RTClib.h"
+
+
+// Pin definitions
+int intPin = 12;  // These can be changed, 2 and 3 are the Arduinos ext int pins
+int myLed  = 9;
+#define LOG_INTERVAL  50 // mills between entries (reduce to take more/faster data)
+#define MPU9250_ADDRESS MPU9250_ADDRESS_AD1
+#define SYNC_INTERVAL 50 // mills between calls to flush() - to write data to the card
+uint32_t syncTime = 0; // time of last sync()
+
+MPU9250 myIMU(MPU9250_ADDRESS,Wire,400000);
+RTC_PCF8523 RTC;
+
+// for the data logging shield, we use digital pin 10 for the SD cs line
+const int chipSelect = 10;
+
+// the logging file
+File logfile;
+
+void error(char *str)
+{
+  Serial.print("error: ");
+  Serial.println(str);
+  
+  
+  while(1);
+}
+
+void setup()
+{
+  Wire.begin();
+  // TWBR = 12;  // 400 kbit/sec I2C speed
+  Serial.begin(38400);
+  //while(!Serial){};
+
+
+  // Set up the interrupt pin, its set as active high, push-pull
+  pinMode(intPin, INPUT);
+  digitalWrite(intPin, LOW);
+  pinMode(myLed, OUTPUT);
+  digitalWrite(myLed, HIGH);
+  
+  // initialize the SD card
+  //Serial.print("Initializing SD card...");
+  // make sure that the default chip select pin is set to
+  // output, even if you don't use it:
+  pinMode(10, OUTPUT);
+  
+  SD.begin(chipSelect);
+  
+  // create a new file
+  char filename[] = "LOGGER00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;  // leave the loop!
+    }
+  }
+      // connect to RTC
+  Wire.begin();  
+  if (!RTC.begin()) 
+  {
+      Serial.println("RTC failed");
+  }
+  
+     logfile.println("millis,datetime,ax,ay,az,gx,gy,gz,mx,my,mz"); 
+/*
+  // Read the WHO_AM_I register, this is a good test of communication
+  byte c = myIMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
+  Serial.print(F("MPU9250 I AM 0x"));
+  Serial.print(c, HEX);
+  Serial.print(F(" I should be 0x"));
+  Serial.println(0x71, HEX);
+*/
+
+
+//  if (c == 0x71) // WHO_AM_I should always be 0x71
+  {
+//    Serial.println(F("MPU9250 is online..."));
+
+    // Start by performing self test and reporting values
+    //myIMU.MPU9250SelfTest(myIMU.selfTest);
+/*    Serial.print(F("x-axis self test: acceleration trim within : "));
+    Serial.print(myIMU.selfTest[0],1); Serial.println("% of factory value");
+    Serial.print(F("y-axis self test: acceleration trim within : "));
+    Serial.print(myIMU.selfTest[1],1); Serial.println("% of factory value");
+    Serial.print(F("z-axis self test: acceleration trim within : "));
+    Serial.print(myIMU.selfTest[2],1); Serial.println("% of factory value");
+    Serial.print(F("x-axis self test: gyration trim within : "));
+    Serial.print(myIMU.selfTest[3],1); Serial.println("% of factory value");
+    Serial.print(F("y-axis self test: gyration trim within : "));
+    Serial.print(myIMU.selfTest[4],1); Serial.println("% of factory value");
+    Serial.print(F("z-axis self test: gyration trim within : "));
+    Serial.print(myIMU.selfTest[5],1); Serial.println("% of factory value");
+*/
+    // Calibrate gyro and accelerometers, load biases in bias registers
+    myIMU.calibrateMPU9250(myIMU.gyroBias, myIMU.accelBias);
+
+
+    myIMU.initMPU9250();
+    // Initialize device for active mode read of acclerometer, gyroscope, and
+    // temperature
+//    Serial.println("MPU9250 initialized for active data mode....");
+
+    // Read the WHO_AM_I register of the magnetometer, this is a good test of
+    // communication
+/*    byte d = myIMU.readByte(AK8963_ADDRESS, WHO_AM_I_AK8963);
+    Serial.print("AK8963 ");
+    Serial.print("I AM 0x");
+    Serial.print(d, HEX);
+    Serial.print(" I should be 0x");
+    Serial.println(0x48, HEX);
+
+
+
+    if (d != 0x48)
+    {
+      // Communication failed, stop here
+      Serial.println(F("Communication failed, abort!"));
+      Serial.flush();
+      abort();
+    }
+*/
+    // Get magnetometer calibration from AK8963 ROM
+    myIMU.initAK8963(myIMU.factoryMagCalibration);
+    // Initialize device for active mode read of magnetometer
+//    Serial.println("AK8963 initialized for active data mode....");
+
+/*
+      //  Serial.println("Calibration values: ");
+      Serial.print("X-Axis factory sensitivity adjustment value ");
+      Serial.println(myIMU.factoryMagCalibration[0], 2);
+      Serial.print("Y-Axis factory sensitivity adjustment value ");
+      Serial.println(myIMU.factoryMagCalibration[1], 2);
+      Serial.print("Z-Axis factory sensitivity adjustment value ");
+      Serial.println(myIMU.factoryMagCalibration[2], 2);
+*/    
+
+
+    // Get sensor resolutions, only need to do this once
+    myIMU.getAres();
+    myIMU.getGres();
+    myIMU.getMres();
+
+    // The next call delays for 4 seconds, and then records about 15 seconds of
+    // data to calculate bias and scale.
+//    myIMU.magCalMPU9250(myIMU.magBias, myIMU.magScale);
+//    Serial.println("AK8963 mag biases (mG)");
+//    Serial.println(myIMU.magBias[0]);
+//    Serial.println(myIMU.magBias[1]);
+//    Serial.println(myIMU.magBias[2]);
+
+//    Serial.println("AK8963 mag scale (mG)");
+//    Serial.println(myIMU.magScale[0]);
+//    Serial.println(myIMU.magScale[1]);
+//    Serial.println(myIMU.magScale[2]);
+//    delay(2000); // Add delay to see results before serial spew of data
+
+  
+//      Serial.println("Magnetometer:");
+//      Serial.print("X-Axis sensitivity adjustment value ");
+//      Serial.println(myIMU.factoryMagCalibration[0], 2);
+//      Serial.print("Y-Axis sensitivity adjustment value ");
+//      Serial.println(myIMU.factoryMagCalibration[1], 2);
+//      Serial.print("Z-Axis sensitivity adjustment value ");
+//      Serial.println(myIMU.factoryMagCalibration[2], 2);
+    
+
+/*
+  } // if (c == 0x71)
+  else
+  {
+    Serial.print("Could not connect to MPU9250: 0x");
+    Serial.println(c, HEX);
+
+    // Communication failed, stop here
+    Serial.println(F("Communication failed, abort!"));
+    Serial.flush();
+    abort();
+  }
+}
+*/
+  }}
+void loop()
+{
+    DateTime now;
+
+  // delay for the amount of time we want between readings
+  delay((LOG_INTERVAL -1) - (millis() % LOG_INTERVAL));
+  
+  // log milliseconds since starting
+  uint32_t m = millis();
+  logfile.print(m);           // milliseconds since start
+  logfile.print(", "); 
+
+  // fetch the time
+  now = RTC.now();
+  // log time
+  //logfile.print(now.unixtime()); // seconds since 1/1/1970
+  //logfile.print(", ");
+  //logfile.print('"');
+  //logfile.print(now.year(), DEC);
+  //logfile.print("/");
+  //logfile.print(now.month(), DEC);
+  //logfile.print("/");
+  //logfile.print(now.day(), DEC);
+  //logfile.print(" ");
+  logfile.print(now.hour(), DEC);
+  logfile.print(":");
+  logfile.print(now.minute(), DEC);
+  logfile.print(":");
+  logfile.print(now.second(), DEC);
+  logfile.print('"');
+
+  
+  // If intPin goes high, all data registers have new data
+  // On interrupt, check if data ready interrupt
+  if (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+  {
+    myIMU.readAccelData(myIMU.accelCount);  // Read the x/y/z adc values
+
+    // Now we'll calculate the accleration value into actual g's
+    // This depends on scale being set
+    myIMU.ax = (float)myIMU.accelCount[0] * myIMU.aRes  - myIMU.accelBias[0];
+    myIMU.ay = (float)myIMU.accelCount[1] * myIMU.aRes  - myIMU.accelBias[1];
+    myIMU.az = (float)myIMU.accelCount[2] * myIMU.aRes  - myIMU.accelBias[2];
+
+    myIMU.readGyroData(myIMU.gyroCount);  // Read the x/y/z adc values
+
+    // Calculate the gyro value into actual degrees per second
+    // This depends on scale being set
+    myIMU.gx = (float)myIMU.gyroCount[0] * myIMU.gRes;
+    myIMU.gy = (float)myIMU.gyroCount[1] * myIMU.gRes;
+    myIMU.gz = (float)myIMU.gyroCount[2] * myIMU.gRes;
+
+    myIMU.readMagData(myIMU.magCount);  // Read the x/y/z adc values
+
+    // Calculate the magnetometer values in milliGauss
+    // Include factory calibration per data sheet and user environmental
+    // corrections
+    // Get actual magnetometer value, this depends on scale being set
+    myIMU.mx = (float)myIMU.magCount[0] * myIMU.mRes
+               * myIMU.factoryMagCalibration[0] - myIMU.magBias[0];
+    myIMU.my = (float)myIMU.magCount[1] * myIMU.mRes
+               * myIMU.factoryMagCalibration[1] - myIMU.magBias[1];
+    myIMU.mz = (float)myIMU.magCount[2] * myIMU.mRes
+               * myIMU.factoryMagCalibration[2] - myIMU.magBias[2];
+  } // if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+
+  // Must be called before updating quaternions!
+  myIMU.updateTime();
+
+  // Sensors x (y)-axis of the accelerometer is aligned with the y (x)-axis of
+  // the magnetometer; the magnetometer z-axis (+ down) is opposite to z-axis
+  // (+ up) of accelerometer and gyro! We have to make some allowance for this
+  // orientationmismatch in feeding the output to the quaternion filter. For the
+  // MPU-9250, we have chosen a magnetic rotation that keeps the sensor forward
+  // along the x-axis just like in the LSM9DS0 sensor. This rotation can be
+  // modified to allow any convenient orientation convention. This is ok by
+  // aircraft orientation standards! Pass gyro rate as rad/s
+  MahonyQuaternionUpdate(myIMU.ax, myIMU.ay, myIMU.az, myIMU.gx * DEG_TO_RAD,
+                         myIMU.gy * DEG_TO_RAD, myIMU.gz * DEG_TO_RAD, myIMU.my,
+                         myIMU.mx, myIMU.mz, myIMU.deltat);
+      
+        // Print acceleration values in milligs!
+        Serial.print("X-accel: "); Serial.print(myIMU.ax*1000);
+        Serial.print(" mg ");
+        Serial.print("Y-accel: "); Serial.print(myIMU.ay*1000);
+        Serial.print(" mg ");
+        Serial.print("Z-accel: "); Serial.print(myIMU.az*1000);
+        Serial.println(" mg");
+
+
+        // Print gyro values in degree/sec
+        Serial.print("X-gyro rate: "); Serial.print(myIMU.gx, 3);
+        Serial.print(" degrees/sec ");
+        Serial.print("Y-gyro rate: "); Serial.print(myIMU.gy, 3);
+        Serial.print(" degrees/sec ");
+        Serial.print("Z-gyro rate: "); Serial.print(myIMU.gz, 3);
+        Serial.println(" degrees/sec");
+
+
+        // Print mag values in degree/sec
+        Serial.print("X-mag field: "); Serial.print(myIMU.mx);
+        Serial.print(" mG ");
+        Serial.print("Y-mag field: "); Serial.print(myIMU.my);
+        Serial.print(" mG ");
+        Serial.print("Z-mag field: "); Serial.print(myIMU.mz);
+        Serial.println(" mG");
+
+        logfile.print(", ");
+        logfile.print((int)1000 * myIMU.ax);
+        logfile.print(", ");
+        logfile.print((int)1000 * myIMU.ay);
+        logfile.print(", ");
+        logfile.print((int)1000 * myIMU.az);
+        
+ /*     
+        Serial.print("ax = ");  Serial.print((int)1000 * myIMU.ax);
+        Serial.print(" ay = "); Serial.print((int)1000 * myIMU.ay);
+        Serial.print(" az = "); Serial.print((int)1000 * myIMU.az);
+        Serial.println(" mg");
+*/
+        logfile.print(", ");
+        logfile.print(myIMU.gx);
+        logfile.print(", ");
+        logfile.print(myIMU.gy);
+        logfile.print(", ");
+        logfile.print(myIMU.gz);
+/*        
+        Serial.print("gx = ");  Serial.print(myIMU.gx, 2);
+        Serial.print(" gy = "); Serial.print(myIMU.gy, 2);
+        Serial.print(" gz = "); Serial.print(myIMU.gz, 2);
+        Serial.println(" deg/s");
+*/
+        logfile.print(", ");
+        logfile.print((int)myIMU.mx);
+        logfile.print(", ");
+        logfile.print((int)myIMU.my);
+        logfile.print(", ");
+        logfile.print((int)myIMU.mz); 
+        logfile.print(", \n"); 
+/*        
+        Serial.print("mx = ");  Serial.print((int)myIMU.mx);
+        Serial.print(" my = "); Serial.print((int)myIMU.my);
+        Serial.print(" mz = "); Serial.print((int)myIMU.mz);
+        Serial.println(" mG");
+
+        Serial.print("q0 = ");  Serial.print(*getQ());
+        Serial.print(" qx = "); Serial.print(*(getQ() + 1));
+        Serial.print(" qy = "); Serial.print(*(getQ() + 2));
+        Serial.print(" qz = "); Serial.println(*(getQ() + 3));
+*/
+/*
+// Define output variables from updated quaternion---these are Tait-Bryan
+// angles, commonly used in aircraft orientation. In this coordinate system,
+// the positive z-axis is down toward Earth. Yaw is the angle between Sensor
+// x-axis and Earth magnetic North (or true North if corrected for local
+// declination, looking down on the sensor positive yaw is counterclockwise.
+// Pitch is angle between sensor x-axis and Earth ground plane, toward the
+// Earth is positive, up toward the sky is negative. Roll is angle between
+// sensor y-axis and Earth ground plane, y-axis up is positive roll. These
+// arise from the definition of the homogeneous rotation matrix constructed
+// from quaternions. Tait-Bryan angles as well as Euler angles are
+// non-commutative; that is, the get the correct orientation the rotations
+// must be applied in the correct order which for this configuration is yaw,
+// pitch, and then roll.
+// For more see
+// http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+// which has additional links.
+      myIMU.yaw   = atan2(2.0f * (*(getQ()+1) * *(getQ()+2) + *getQ()
+                    * *(getQ()+3)), *getQ() * *getQ() + *(getQ()+1)
+                    * *(getQ()+1) - *(getQ()+2) * *(getQ()+2) - *(getQ()+3)
+                    * *(getQ()+3));
+      myIMU.pitch = -asin(2.0f * (*(getQ()+1) * *(getQ()+3) - *getQ()
+                    * *(getQ()+2)));
+      myIMU.roll  = atan2(2.0f * (*getQ() * *(getQ()+1) + *(getQ()+2)
+                    * *(getQ()+3)), *getQ() * *getQ() - *(getQ()+1)
+                    * *(getQ()+1) - *(getQ()+2) * *(getQ()+2) + *(getQ()+3)
+                    * *(getQ()+3));
+      myIMU.pitch *= RAD_TO_DEG;
+      myIMU.yaw   *= RAD_TO_DEG;
+
+      // Declination of SparkFun Electronics (40°05'26.6"N 105°11'05.9"W) is
+      // 	8° 30' E  ± 0° 21' (or 8.5°) on 2016-07-19
+      // - http://www.ngdc.noaa.gov/geomag-web/#declination
+      myIMU.yaw  -= 8.5;
+      myIMU.roll *= RAD_TO_DEG;
+
+     
+        Serial.print("Yaw, Pitch, Roll: ");
+        Serial.print(myIMU.yaw, 2);
+        Serial.print(", ");
+        Serial.print(myIMU.pitch, 2);
+        Serial.print(", ");
+        Serial.println(myIMU.roll, 2);
+
+        Serial.print("rate = ");
+        Serial.print((float)myIMU.sumCount / myIMU.sum, 2);
+        Serial.println(" Hz");
+  */    
+
+
+    // With these settings the filter is updating at a ~145 Hz rate using the
+    // Madgwick scheme and >200 Hz using the Mahony scheme even though the
+    // display refreshes at only 2 Hz. The filter update rate is determined
+    // mostly by the mathematical steps in the respective algorithms, the
+    // processor speed (8 MHz for the 3.3V Pro Mini), and the magnetometer ODR:
+    // an ODR of 10 Hz for the magnetometer produce the above rates, maximum
+    // magnetometer ODR of 100 Hz produces filter update rates of 36 - 145 and
+    // ~38 Hz for the Madgwick and Mahony schemes, respectively. This is
+    // presumably because the magnetometer read takes longer than the gyro or
+    // accelerometer reads. This filter update rate should be fast enough to
+    // maintain accurate platform orientation for stabilization control of a
+    // fast-moving robot or quadcopter. Compare to the update rate of 200 Hz
+    // produced by the on-board Digital Motion Processor of Invensense's MPU6050
+    // 6 DoF and MPU9150 9DoF sensors. The 3.3 V 8 MHz Pro Mini is doing pretty
+    // well!
+
+
+      myIMU.count = millis();
+      myIMU.sumCount = 0;
+      myIMU.sum = 0;
+
+      if ((millis() - syncTime) < SYNC_INTERVAL) return;
+      syncTime = millis();
+    
+      logfile.flush();
+}
+   
